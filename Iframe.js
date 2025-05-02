37abddc0434e95ce91c783feb2060e0e32d1d5dd
Ichlyx <gweeks@student.escwr.org>
2025-05-02 10:46:11 -0400

    Update Iframe.js

--- Iframe.js
+++ Iframe.js
@@ -1,6 +1,6 @@
 javascript:(function() {
   var iframe = document.createElement('iframe');
-  iframe.setAttribute('src', 'YOUR SITE HERE');
+  iframe.setAttribute('src', 'google.com');
   iframe.style.position = 'fixed';
   iframe.style.top = '50%';
   iframe.style.left = '50%';
